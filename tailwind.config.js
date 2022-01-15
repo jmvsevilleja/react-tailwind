d906a699c78f1d24812338af7f277ecab3ef145f
Jess Mark Sevilleja <jess@clinica.dev>
2022-01-16 00:36:58 +0800

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ff4800",
+      }
+    },
   },
   plugins: [],
 }
